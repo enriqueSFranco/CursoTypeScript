10d210dc8170ffff16f6ff3a4a00750380d9b986
Enrique SFranco <enrique.sfranco04@gmail.com>
2023-04-13 14:14:56 -0600

    Setup project

---
+++ src/index.ts
@@ -0,0 +1,13 @@
+/* TIPOS DE DATO */
+let ageOfExtinction: number = 76_000_000
+let transformer: string = 'optimus'
+
+/* interface User {
+  id: number
+  name: string,
+}
+
+const user:User = {
+  id: 0,
+  name: 'kike'
+} */
