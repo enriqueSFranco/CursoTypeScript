e309e4ce68f50124361e989b76f26139ec149590
Enrique SFranco <enrique.sfranco04@gmail.com>
2023-04-21 16:24:09 -0600

    friday 13

--- src/algoritmos/viernes13.ts
+++ src/algoritmos/viernes13.ts
@@ -2,3 +2,16 @@
  * Crea una función que sea capaz de detectar si existe un viernes 13 en el mes y el año indicados.
  * - La función recibirá el mes y el año y retornará verdadero o falso.
  */
+
+interface Params {
+  month: number
+  year: number
+}
+
+function friday13({year, month}: Params) {
+  const FRIDAY = 5
+  const FRIDAY_13 = 13
+  return new Date(year, month - 1, FRIDAY_13).getDay() === FRIDAY
+}
+
+console.log(friday13({year: 2023, month: 1}))
