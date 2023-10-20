d9b9b66a30c36fc532d584bfd948b51a1b9e4785
enriqueSFranco <enrique.sfranco04@gmail.com>
2023-10-20 13:29:14 -0600

    feat: add dependencie vitest and configuration

--- entrevista06/vite.config.ts
+++ entrevista06/vite.config.ts
@@ -1,6 +1,11 @@
+/// <reference types="vitest" />
+
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
 export default defineConfig({
-  plugins: [react()]
+  plugins: [react()],
+  test: {
+    environment: "happy-dom"
+  }
 })
