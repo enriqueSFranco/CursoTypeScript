985efd1c08b8ea78e031f107809dc816ebfaa5c3
enriqueSFranco <enrique.sfranco04@gmail.com>
2023-10-20 11:23:50 -0600

    feat: nueva estrategia para recuperar el valor de un input

--- entrevista06/src/App.tsx
+++ entrevista06/src/App.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useMemo, useState } from 'react'
 
 type ItemId = `${string}-${string}-${string}-${string}-${string}`
 
@@ -16,6 +16,7 @@ const ITEMS: Item[] = [
 
 export function App () {
   const [items, updateItems] = useState<Item[]>(ITEMS)
+  const totalItems = useMemo(() => items.length, [items.length])
 
   function addItem (text: string) {
     const newItem: Item = {
@@ -28,33 +29,53 @@ export function App () {
 
   function handleSubmit (e: React.FormEvent<HTMLFormElement>) {
     e.preventDefault()
-    const $form = e.target as HTMLFormElement
-    const formData = new FormData($form)
-    const field = formData.get('item') as string
+    // MARK: Estrategia #1
+    // const $form = e.target as HTMLFormElement
+    // const formData = new FormData($form)
+    // const field = formData.get('item') as string
 
-    if (!field.trim().length) return
+    // if (!field.trim().length) return
 
-    addItem(field)
-    $form.reset()
+    // addItem(field)
+    // $form.reset()
+
+    // MARK: Estrategia #2
+    const { elements } = e.currentTarget
+    const input = elements.namedItem('item')
+
+    const isInput = input instanceof HTMLInputElement
+
+    if (!isInput || input === null) return
+
+    addItem(input.value)
+    input.value = ''
   }
 
   function deleteItem (id: ItemId) {
-    console.log('remove', id)
+    updateItems(prevItems => {
+      const updatedItems = prevItems.filter(item => item.id !== id)
+      return updatedItems
+    })
   }
 
   return (
     <main>
-      <h1>Entrevista técnica #6</h1>
-      <form className="add-item-form" onSubmit={handleSubmit}>
-        <label htmlFor="item" className="label-item">
-          <span>Item</span>
-          <input type="text" name="item" placeholder="videojuegos" />
-        </label>
-        <button>Add Item</button>
-      </form>
+      <section>
+        <header>
+          <h1>Entrevista técnica #6</h1>
+        </header>
+        <form className="add-item-form" onSubmit={handleSubmit}>
+          <label htmlFor="item" className="label-item">
+            <span>Item</span>
+            <input type="text" name="item" placeholder="videojuegos" autoComplete="off" />
+          </label>
+          <button>Add Item</button>
+        </form>
+      </section>
 
       {/* TODO: LIST ITEMS */}
-      <article className="container">
+      <article className="content">
+        <h2>Items: {totalItems}</h2>
         <ul className="item-list">
           {items.map(item => (
             <li key={`item-id-${item.id}`}><span onClick={() => deleteItem(item.id)}>{item.text}</span></li>
