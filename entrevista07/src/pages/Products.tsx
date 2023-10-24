ee2f390b271c861b06fa5926894c731342f0d308
enriqueSFranco <enrique.sfranco04@gmail.com>
2023-10-23 19:06:33 -0600

    feaT: add view show products

--- entrevista07/src/pages/Products.tsx
+++ entrevista07/src/pages/Products.tsx
@@ -1,18 +1,20 @@
+import { ProductList } from '../components/ui/ProductList'
+
 export const Products = () => {
   return (
     <section>
       <header>
         <h2>logo</h2>
-        <form action="">
+        <form>
           <div>
-            <input type="text" placeholder="laptops, smartphones" />
+            <input type='text' placeholder='laptops, smartphones' />
           </div>
           <button>buscar</button>
         </form>
       </header>
 
       <div>
-        <h3>resultados de busqueda de "smart": 12</h3>
+        <h3>resultados de busqueda de 'smart': 12</h3>
         <ul>
           <li><span>smartphones: 5</span></li>
           <li><span>fragances: 2</span></li>
@@ -20,9 +22,7 @@ export const Products = () => {
       </div>
 
       <div>
-        <ul>
-          <li><span>product</span></li>
-        </ul>
+        <ProductList />
       </div>
     </section>
   )
