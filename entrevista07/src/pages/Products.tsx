b74f78640dcebe5f19e26f5850f0f682ee0d8723
enriqueSFranco <enrique.sfranco04@gmail.com>
2023-10-23 18:55:17 -0600

    feat: add views

---
+++ entrevista07/src/pages/Products.tsx
@@ -0,0 +1,29 @@
+export const Products = () => {
+  return (
+    <section>
+      <header>
+        <h2>logo</h2>
+        <form action="">
+          <div>
+            <input type="text" placeholder="laptops, smartphones" />
+          </div>
+          <button>buscar</button>
+        </form>
+      </header>
+
+      <div>
+        <h3>resultados de busqueda de "smart": 12</h3>
+        <ul>
+          <li><span>smartphones: 5</span></li>
+          <li><span>fragances: 2</span></li>
+        </ul>
+      </div>
+
+      <div>
+        <ul>
+          <li><span>product</span></li>
+        </ul>
+      </div>
+    </section>
+  )
+}
